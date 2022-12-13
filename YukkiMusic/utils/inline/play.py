f9e70d41e267c0eb53fb8e39e05906517cbb7d41
JerryBotz <113348062+JerryBotz@users.noreply.github.com>
2022-12-13 19:19:46 +0530

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -77,7 +77,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         [
             InlineKeyboardButton(
                        text="📡Join",
-                       url='https://t.me/Ayraupdates'), 
+                       url='https://t.me/WorldchattingGroup_x'), 
             InlineKeyboardButton(
                 text=_["CLOSEMENU_BUTTON"], callback_data="close"
             )
@@ -140,7 +140,7 @@ def stream_markup(_, videoid, chat_id):
         [
             InlineKeyboardButton(
                        text="📡Join",
-                       url='https://t.me/Ayraupdates'), 
+                       url='https://t.me/WorldchattingGroup_x'), 
             InlineKeyboardButton(
                 text=_["CLOSEMENU_BUTTON"], callback_data="close"
             )
